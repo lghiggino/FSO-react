ed00ed240dfac7bf3cb6e6520c1313ab6a5411c0
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-30 11:28:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react"
 
-function App() {
+const Header = (props) => {
+  return (  
+    <h1>{props.course}</h1>
+  )
+}
+
+const NewPart = (props)=> {
+  // console.log("from newPart::",props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+   console.log("from content::",props)
+  return (
+    <>
+      {console.log(props.parts.length)}
+      <NewPart name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <NewPart name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <NewPart name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  )  
+}
+
+const Total = (props) => {
+  // console.log("from total::",props)
+  return (
+    <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamental of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      }
+    ]
+  }
+ 
+
+  return (
+    <>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts}/>
+    </>
+  )
 }
 
 export default App;
+
+
