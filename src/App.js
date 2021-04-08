e0266ff5ebd662cab186ecba5d0cfce7ba6fa9cc
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-04-07 22:06:22 -0300

    unicafe and quotes

--- src/App.js
+++ src/App.js
@@ -6,97 +6,52 @@ const Button = ( {handleClick, text}) => {
     )
 }
 
-const LineStatistics = (props) => {
-    return (
-        <li>{props.text}: {props.value}</li>
-    )
-}
 
-const TableLine = (props) => {
-    return (
-        <tr>
-            <td>{props.statTitle}</td>
-            <td>{props.statValue}</td>
-        </tr>
-    )
-}
-
-const MainStats = (props) => {
-    return (
-        <table>
-            <tbody>
-                <TableLine statTitle={"good"} statValue={props.good}/>
-                <TableLine statTitle={"neutral"} statValue={props.neutral}/>
-                <TableLine statTitle={"bad"} statValue={props.bad}/>
-                <TableLine statTitle={"all"} statValue={props.all}/>
-                <TableLine statTitle={"average"} statValue={props.average}/>
-                <TableLine statTitle={"positive"} statValue={props.positive}/>
-            </tbody>
-        </table>
-    )
-}
 
 const App = () => {
     //states
-    const [clicks, setClicks] = useState (
-            {
-                good: 0,
-                neutral: 0,
-                bad: 0,
-            })
-    
+    const [vote, setVote] = useState([0,0,0,0,0,0])
+    const [selected, setSelected] = useState(0)
+
     //variables 
-    let goodF = 0
-    let neutralF = 0
-    let badF = 0
-    const allClicks = (clicks.good + clicks.neutral + clicks.bad)
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    ]
 
     //state handlers
-    const handleGoodClick = () => {
-        setClicks({...clicks, good: clicks.good + 1})
-        goodF = goodF + 1
+    const generateRandomNumber = () => {
+        const randomNumber = Math.floor(Math.random() * anecdotes.length)
+        return randomNumber
     }
-    const handleNeutralClick = () => {
-        setClicks({...clicks, neutral: clicks.neutral + 1})
-        neutralF = neutralF + 1
+    
+    const generateAnecdote = () => {
+        const randomNumber = generateRandomNumber()
+        setSelected(randomNumber) 
+        manageUpvote(randomNumber)
     }
-    const handleBadClick = () => {
-        setClicks({...clicks, bad: clicks.bad + 1})
-        badF = badF + 1
+
+    const manageUpvote = (number) => {
+        console.log(number)
 
     }
 
     //render
     return(
         <>
-            <h3>give feedback</h3>
-
-            <Button handleClick={handleGoodClick} text={"Good"} />
-            <Button handleClick={handleNeutralClick} text={"Neutral"} />
-            <Button handleClick={handleBadClick} text={"Bad"} />
-
-            <div>
-            <h3>statistics</h3>
-
-            {/* <ul>
-                {allClicks === 0 ? 
-                    <li>{"No Feedback given"}</li> : 
-                    <>
-                        <LineStatistics text={"good"} value={clicks.good}/>
-                        <LineStatistics text={"neutral"} value={clicks.neutral}/>
-                        <LineStatistics text={"bad"} value={clicks.bad}/>
-                        <LineStatistics text={"all"} value={allClicks}/>
-                        <LineStatistics text={"average"} value={(clicks.good - clicks.bad / allClicks) || 0}/>
-                        <LineStatistics text={"positive"} value={(((clicks.good/allClicks) || 1) * 100).toFixed(2)}/>
-                    </>
-                    }
-                
-            </ul> */}
-            </div>
-            {allClicks === 0 ? 
-                <p>{"No Feedback given"}</p> :
-                <MainStats good={clicks.good} neutral={clicks.neutral} bad={clicks.bad} all={allClicks} average={(clicks.good - clicks.bad / allClicks) || 0} positive={(((clicks.good/allClicks) || 1) * 100).toFixed(2)}/>
-            }
+            <section>
+                {anecdotes[selected]}
+            </section>
+            <section>
+                <Button handleClick={generateAnecdote} text={"next anecdote"}></Button>
+                <Button handleClick={upvote} text={"vote"}></Button>
+            </section>
+            
+            
         </> 
         
     )
