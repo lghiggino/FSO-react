bed15cde5c191cd38a6b722f63e924f162167d42
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-04-25 17:49:34 -0300

    part 2 - ex 2-2

--- partTwo/src/App.js
+++ partTwo/src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 const Course = ({ course }) => {
   console.log(course)
+  let totalExercises = course.parts.reduce( (acc, curr) => {
+    // console.log("what is happening?")
+    // console.log("acc", acc)
+    // console.log("curr", curr)
+    return acc + curr.exercises
+  }, 0)
   return(
     <div>
       {course.parts.map( crs => (
@@ -10,9 +16,12 @@ const Course = ({ course }) => {
           <span>{crs.exercises}</span>
         </section>
       ))}
+      <div>total of: {totalExercises} exercises</div>
     </div>
   )
 }
+
+
 
 const App = () => {
   const course = {
@@ -33,6 +42,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
