7e5b117d8a87171af0c349138fc09a7393497ed7
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-04-24 22:09:45 -0300

    2.1 solved

--- partTwo/src/App.js
+++ partTwo/src/App.js
@@ -1,20 +1,47 @@
 import React from 'react'
-import Note from "./components/Note"
 
-const App = ({ notes }) => {
+const Course = ({ course }) => {
+  console.log(course)
+  return(
+    <div>
+      {course.parts.map( crs => (
+        <section key={crs.id}>
+          <span>{crs.name} </span>
+          <span>{crs.exercises}</span>
+        </section>
+      ))}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <footer>
-        this is a footer with extra juice
-      </footer>
-    </div>
+    <>
+      <h1>{course.name}</h1>
+      <Course course={course} />
+    </>
   )
 }
 
