bd3cd7335753db5162c96dab8e4a0732e7c8ce03
lghiggino <lghiggino@gmail.com>
2021-06-22 07:52:50 -0300

    array.map experiments

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,28 @@
+const subjects = [
+  {name: "George", age: "25"},
+  {name: "Daisy", age: "21"},
+  {name: "Emerald", age: "46"},
+]
+
+const Hello = ( {name, age} ) => {
+  return(
+    <p>Hello {name}! You are {age} years old.</p>
+  )
+}
+
 function App() {
+  const now = new Date().toDateString()
+  const a = 10
+  const b = 20
   return (
     <div>
-      <p>Hello World</p>
+      {subjects.map( (el,idx) => 
+        <Hello key={idx} name={el.name} age={el.age}/>
+      )}
+      <p>
+        {a} plus {b} is equal to {a + b}
+      </p>
+      <p>Today is: {now} </p>
     </div>
   );
 }
