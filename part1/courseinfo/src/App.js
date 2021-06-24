07c9aebb49bc6d1e8ad97d75c4f16f9b4296637a
lghiggino <lghiggino@gmail.com>
2021-06-24 09:26:58 -0300

    experimental course flow

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,43 +1,76 @@
 import React from 'react'
 
-const Header = ({course}) => {
-  return(
-    <h1>{course}</h1>
-  )
+// const Header = ({course}) => {
+//   return(
+//     <h1>{course}</h1>
+//   )
+// }
+
+// const Content = ({parts}) => {
+//   return(
+//     <>
+//       {parts.map(el => <p>{el.name}:  {el.exercises}</p>)}
+//     </>
+//   )
+// }
+
+// const Total = ({parts}) => {
+//   return (
+//     <>
+//     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+//     <p>Number of exercises {parts.reduce((acc, curr) => (console.log("acc", acc.exercises, curr.exercises), 0))}</p>
+//     <p>{[1,2,3].reduce((acc,curr) => (acc + curr), 0)}</p>
+//     </>
+//   )
+// }
+
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+
+//     const parts = [
+//     { name: 'Fundamentals of React', exercises: 10},
+//     { name: 'Using props to pass data', exercises: 7},
+//     { name: 'State of a component', exercises: 14 }
+//   ]
+
+//   return (
+//     <div>
+//       <Header course={course}/>
+//       <Content parts = {parts}/>
+//       <Total parts={parts}/>
+//     </div>
+//   )
+// }
+
+
+const bornYear = (props) => {
+  console.log(props)
+  const yearNow = new Date().getFullYear()
+  return yearNow - props.age
 }
 
-const Content = ({parts}) => {
-  return(
-    <>
-      {parts.map(el => <p>{el.name}  {el.exercises}</p>)}
-    </>
-  )
-}
 
-const Total = ({parts}) => {
+const Hello = (props) => {
+  
   return (
-    <>
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    <p>Number of exercises {parts.reduce((acc, curr) => (console.log("acc", acc.exercises, curr.exercises), 0))}</p>
-    </>
+    <div>
+      <p> Hello {props.name}, you are {props.age}</p>
+      <p> So you were probably born on {bornYear(props)}</p>
+    </div>
   )
 }
-
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-    const parts = [
-    { name: 'Fundamentals of React', exercises: 10},
-    { name: 'Using props to pass data', exercises: 7},
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const name = "Peter"
+  const age = 10
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts = {parts}/>
-      <Total parts={parts}/>
+      <h1>Greetings</h1>
+      <Hello name="Leonardo" age="40" />
+      <Hello name="Joan" age="22" />
+      <Hello name={name} age={age}/>
     </div>
   )
 }
