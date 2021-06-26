d8d48ba9379add8d89b076140a42845b5bb1cbaf
lghiggino <lghiggino@gmail.com>
2021-06-26 20:18:09 -0300

    1.9 unicafe step4 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,62 @@
-import react, { useState } from "react"
+import { useState } from "react"
 
 //syling, because why not?
 import './App.css';
+
+const CalcAverage = ({ good, bad, allVotes }) => {
+  return (
+    <p>average: {good - bad / allVotes}</p>
+  )
+}
+
+const CalcPosititve = ({ good, allVotes }) => {
+  return (
+    <p>positives: {(good / allVotes) * 100}%</p>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, allVotes }) => {
+  return (
+    <div>
+      <h2> Statistics</h2>
+      {!allVotes ?
+        <>
+          <p>No feedback given</p>
+        </>
+        :
+        <>
+          <p>good: {good}</p>
+          <p>neutral: {neutral}</p>
+          <p>bad: {bad}</p>
+          <p>all: {allVotes}</p>
+          <CalcAverage good={good} bad={bad} allVotes={allVotes} />
+          <CalcPosititve good={good} allVotes={allVotes} />
+        </>
+      }
+
+    </div>
+  )
+}
 
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allVotes, setAllVotes] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAllVotes(allVotes + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAllVotes(allVotes + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAllVotes(allVotes + 1)
   }
 
   return (
@@ -29,13 +68,7 @@ function App() {
         <button onClick={handleBad}>bad</button>
       </div>
 
-
-      <div>
-        <h2> Statistics</h2>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} allVotes={allVotes} />
 
     </section>
   );
