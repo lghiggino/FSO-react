cb229867f3e6c17fc88a458858acb76ef31ada43
lghiggino <lghiggino@gmail.com>
2021-07-02 08:49:27 -0300

    phonebook 2.5 works

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas" }
   ])
-  const [newName, setNewName] = useState("a new name")
+  const [newName, setNewName] = useState("a new name...")
 
   const handlePersonChange = (event) => {
     console.log(event.target.value)
@@ -16,6 +16,7 @@ function App() {
     event.preventDefault()
     console.log("clicou em add", event.target)
     setPersons(persons.concat({"name": newName}))
+    setNewName("")
   }
 
   return (
@@ -40,26 +41,42 @@ function App() {
 
 // import Note from './components/Note';
 
-// const addNote = (event) => {
-//   event.preventDefault()
-//   console.log("save note button was clicked", event.target)
-// }
+
 
 // const App = (props) => {
 //   const [notes, setNotes] = useState(props.notes)
+//   const [newNote, setNewNote] = useState("a new note")
+
+//   const addNote = (event) => {
+//     event.preventDefault()
+//     console.log("save note button was clicked", event.target)
+//     const noteObject = {
+//       content: newNote,
+//       date: new Date().toISOString(),
+//       important: Math.random() < 0.3,
+//       id: notes.length + 1
+//     }
+//     setNotes(notes.concat(noteObject))
+//     setNewNote("")
+//   }
+
+//   const handleNoteChange = (event) => {
+//     console.log(event.target.value)
+//     setNewNote(event.target.value)
+//   }
 
 //   return (
 //     <div className="App">
 //       <h1>Notes</h1>
 //       <ul>
-//         {notes.map(note => 
-//           <Note key={note.id} note={note}/>)}
+//         {notes.map(note =>
+//           <Note key={note.id} note={note} />)}
 //       </ul>
 //       <form onSubmit={addNote}>
-//         <input />
+//         <input value={newNote} onChange={handleNoteChange} />
 //         <button type="submit">save note</button>
 //       </form>
-     
+
 //     </div>
 //   )
 // }
