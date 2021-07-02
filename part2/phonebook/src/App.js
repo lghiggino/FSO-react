7b124b1eb5c7e5d182392e9ec137c7b359cf08e0
lghiggino <lghiggino@gmail.com>
2021-07-02 08:41:00 -0300

    2.5 makes sens - keep forms clean

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,62 +1,68 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// function App() {
-//   const [persons, setPersons] = useState([
-//     { name: "Arto Hellas" }
-//   ])
-//   const [newName, setNewName] = useState("")
+function App() {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" }
+  ])
+  const [newName, setNewName] = useState("a new name")
 
-//   const handlePersonChange = (ev) => {
-//     console.log(ev.target.value)
-//   }
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
-//   const addPerson = (ev) => {
-//     ev.preventDefault()
-//     console.log("clicou em add", ev.target)
-//   }
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log("clicou em add", event.target)
+    setPersons(persons.concat({"name": newName}))
+  }
+
+  return (
+    <div className="App">
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+          <label>name:</label>
+          <input value={newName} onChange={handlePersonChange}/>
+          <button type="submit">add</button>
+          debug: {newName}
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+}
+
+
+/* NOTES */
+
+// import Note from './components/Note';
+
+// const addNote = (event) => {
+//   event.preventDefault()
+//   console.log("save note button was clicked", event.target)
+// }
+
+// const App = (props) => {
+//   const [notes, setNotes] = useState(props.notes)
 
 //   return (
 //     <div className="App">
-//       <h2>Phonebook</h2>
-//       <form onSubmit={addPerson}>
-//         <div>
-//           <label>name:</label>
-//           <input
-//             // onChange={handlePersonChange}
-
-//           />
-//         </div>
-//         <div>
-//           <button type="submit">add</button>
-//         </div>
-//         <div>
-//           debug: {newName}
-//         </div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => 
+//           <Note key={note.id} note={note}/>)}
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input />
+//         <button type="submit">save note</button>
 //       </form>
-//       <h2>Numbers</h2>
-//       {persons.map(person => (
-//         <p key={person.name}>{person.name}</p>
-//       ))}
+     
 //     </div>
-//   );
-
-const App = (props) => {
-  console.log(props)
-  const [notes, setNotes] = useState(props.notes)
-  console.log(notes)
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note =>
-          <p key={note.id}>{note.text}</p>
-        )}
-      </ul>
-    </div>
-  )
-}
+//   )
 // }
+
 
 export default App;
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 
 const notes = [
-  {id:1, text: "hello, hello"},
-  {id:2, text: "I dont know why you say goodbye"},
-  {id:3, text: "And I say hello"}
+  {id: 1,
+  content: "HTML is easy",
+  date:"2019-05-30T17:30:31.098z",
+  important: true},
+  {id: 2,
+  content: "Browser can execute only JavaScript",
+  date:"2019-05-30T18:39:31.091z",
+  important: false},
+  {id: 3,
+  content: "GET and POST are the most important methods of HTTP protocol",
+  date:"2019-05-30T19:20:31.298z",
+  important: true},
 ]
 
 ReactDOM.render(
