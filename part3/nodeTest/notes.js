18ae0749166b3b48e994fc69796336547a1e3670
lghiggino <lghiggino@gmail.com>
2021-07-18 09:09:41 -0300

    installed express

---
+++ part3/nodeTest/notes.js
@@ -0,0 +1,52 @@
+let notes = [
+    {
+        "id": 1,
+        "content": "HTML is easy",
+        "date": "2019-05-30T17:30:31.098z",
+        "important": false
+    },
+    {
+        "id": 2,
+        "content": "Browser can execute only JavaScript",
+        "date": "2019-05-30T18:39:31.091z",
+        "important": true
+    },
+    {
+        "id": 3,
+        "content": "GET and POST are the most important methods of HTTP protocol",
+        "date": "2019-05-30T19:20:31.298z",
+        "important": false
+    },
+    {
+        "id": 4,
+        "content": "Fundamentals of React",
+        "date": "2021-07-05T19:20:31.298z",
+        "important": true
+    },
+    {
+        "content": "JSON server is very useful",
+        "date": "2021-07-11T11:16:50.439Z",
+        "important": false,
+        "id": 5
+    },
+    {
+        "content": "this is a test note",
+        "date": "2021-07-08T23:55:21.670Z",
+        "important": true,
+        "id": 6
+    },
+    {
+        "content": "This is a new test note",
+        "date": "2021-07-08T23:56:06.112Z",
+        "important": true,
+        "id": 7
+    },
+    {
+        "content": "This note serves to check the id continuity",
+        "date": "2021-07-09T00:04:37.983Z",
+        "important": false,
+        "id": 8
+    }
+]
+
+module.exports = notes
