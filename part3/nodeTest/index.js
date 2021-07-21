7eac46fe7f241a17ef76d2daec0ef6c4fc8faf55
lghiggino <lghiggino@gmail.com>
2021-07-20 21:24:13 -0300

    fully finished app.post

--- part3/nodeTest/index.js
+++ part3/nodeTest/index.js
@@ -35,9 +35,25 @@ app.delete("/api/notes/:id", (request,response) => {
 })
 
 //adding a new note
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
 app.post("/api/notes", (request,response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+
+    if(!body.content){
+        return response.status(400).json({error: "content missing"})
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+   
+    notes = notes.concat(note)
     response.json(note)
 })
 
