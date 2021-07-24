b26611c584c7778f9fe74771899e6962db4104da
lghiggino <lghiggino@gmail.com>
2021-07-24 18:41:20 -0300

    hookup live real heroku backend

--- part2/phonebook/src/services/notes.js
+++ part2/phonebook/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3001/api/notes"
+const baseUrl = "https://peaceful-crag-14176.herokuapp.com/api/notes"
 
 const getAll = () => {
     const request = axios.get(baseUrl)
